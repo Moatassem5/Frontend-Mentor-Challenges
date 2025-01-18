b098a564bafb548924f9d97cb71ee6acf33b69fb
Moatassem Ahmed <moatassemahmed582@gmail.com>
2025-01-18 23:10:12 +0200

    making carousel UI by tailwind 🥳

--- Conference ticket generator/conference-ticket-generator/src/App.tsx
+++ Conference ticket generator/conference-ticket-generator/src/App.tsx
@@ -2,14 +2,14 @@ function App() {
   return (
     <>
       <div className=" flex relative mx-auto my-48 w-[800px] px-36 py-20 rounded-lg bg-teal-100  ">
-        <button className=" absolute left-[-15px] bottom-10 bg-cyan-400 rounded-full p-4">
+        <button className=" delay-20 transition-all ease-in-out group focus:outline-none focus:ring focus:ring-blue-600 absolute left-[-15px] bottom-10 bg-cyan-400 rounded-full p-4 hover:bg-blue-800 hover:translate-x-[-4px]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="2"
             stroke="#03045e"
-            class="size-6"
+            class="size-6 group-hover:stroke-slate-100"
           >
             <path
               stroke-linecap="round"
@@ -18,14 +18,14 @@ function App() {
             />
           </svg>
         </button>
-        <button className="absolute right-[-15px] bottom-10 bg-cyan-400 rounded-full p-4">
+        <button className=" ease-in-out focus:outline-none focus:ring focus:ring-blue-600 group absolute right-[-15px] bottom-10 bg-cyan-400 rounded-full p-4 hover:bg-blue-800 transition-all hover:delay-20 hover:translate-x-[4px]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="2"
-            stroke="#03045e"
-            class="size-6"
+            stroke="#03045e "
+            class="size-6 group-hover:stroke-slate-100"
           >
             <path
               stroke-linecap="round"
