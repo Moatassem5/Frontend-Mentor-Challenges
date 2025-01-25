5b101a73e8771b527763dbcf1d147f2ccc5c8a8c
Moatassem Ahmed <moatassemahmed582@gmail.com>
2025-01-25 15:06:49 +0200

    Making the Header Component

--- Conference ticket generator/conference-ticket-generator/src/components/Header.tsx
+++ Conference ticket generator/conference-ticket-generator/src/components/Header.tsx
@@ -1,10 +1,20 @@
 const Header = function () {
   return (
-    <img
-      src="./public/assets/images/logo-full.svg"
-      className="mx-auto mt-4"
-      alt="Logo"
-    />
+    <>
+      <img
+        src="./public/assets/images/logo-full.svg"
+        className="mx-auto mt-7 w-[40%]  md:w-[13%]"
+        alt="Logo"
+      />
+
+      <h1 className="text-white font-bold text-balance text-[30px] lg:text-[50px] md:text-[35px] mt-10 text-center md:leading-[3.3rem]">
+        Your Journey to Coding Conf <br /> 2025 Starts Here!
+      </h1>
+
+      <p className="text-neutral-200 text-center text-balance mt-7 font-light md:text-[20px]">
+        Secure your spot at next year's biggest coding conference.
+      </p>
+    </>
   );
 };
 
